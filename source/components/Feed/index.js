e8ae741552714354528189e5929250cef2bb063d
olegleshiy <olegleshiy7@gmail.com>
2019-06-15 00:07:02 +0300

    homework-6

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,6 @@
 //Core
 import React, { Component } from 'react';
+import moment from 'moment';
 
 //Instruments
 import StatusBar from '../StatusBar';
@@ -9,28 +10,116 @@ import Spinner from '../Spinner';
 
 //Instruments
 import Styles from './styles.m.css';
+import { getUniqueID, delay } from "../../instruments";
 
 export default class Feed extends Component{
+    constructor () {
+        super();
+
+        this._createPost = this._createPost.bind(this);
+        this._setDownloadDOMState = this._setDownloadDOMState.bind(this);
+        this._likePost = this._likePost.bind(this);
+        this._removePost = this._removePost.bind(this);
+    }
+
     state = {
         posts: [
-            { id: '123', comment: 'Hi there!', created: 1559952000 },
-            { id: '345', comment: 'Hello!', created: 1559952000 }
+            {
+                id: '123',
+                comment: 'Hi there!',
+                created: 1559952000,
+                likes: []
+            },
+            {
+                id: '345',
+                comment: 'Hello!',
+                created: 1559952000,
+                likes: []
+            }
         ],
-        isDownloadDOM: true,
+        isDownloadDOM: false,
     };
+
+    _setDownloadDOMState (state) {
+        this.setState({
+            isDownloadDOM: state,
+        })
+    }
+
+    async _createPost (comment) {
+        this._setDownloadDOMState(true);
+
+        const post = {
+            id:       getUniqueID(),
+            created:  moment.utc(),
+            comment,
+            likes: [],
+        };
+
+        await delay(1200);
+
+        this.setState(({ posts }) => ({
+            posts: [post, ...posts],
+            isDownloadDOM: false,
+        }));
+    }
+
+    async _likePost (id) {
+        const { currentUserFirstName, currentUserLastName } = this.props;
+        this._setDownloadDOMState(true);
+
+        await delay(1200);
+
+        const newPost = this.state.posts.map(post => {
+            if (post.id === id) {
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            id:        getUniqueID(),
+                            firstName: currentUserFirstName,
+                            lastName:  currentUserLastName,
+                        }
+                    ]
+                }
+            }
+            return post;
+        });
+
+        this.setState({
+            posts:         newPost,
+            isDownloadDOM: false,
+        });
+    };
+
+    async _removePost (id) {
+        this._setDownloadDOMState(true);
+
+        await delay(1200);
+
+        this.setState(({posts}) => ({
+            posts:         posts.filter((post) => post.id !== id),
+            isDownloadDOM: false,
+        }));
+    }
 
     render() {
         const { posts, isDownloadDOM } = this.state;
 
         const postsJSX = posts.map((post) => {
-            return <Post key = { post.id } { ...post } />
+            return <Post
+                key = { post.id }
+                { ...post }
+                _likePost = { this._likePost }
+                _removePost = { this._removePost }
+            />
         });
 
         return (
             <section className = { Styles.feed }>
-                <Spinner isSpinning = { isDownloadDOM }  />
+                <Spinner isSpinning = { isDownloadDOM } />
                 <StatusBar />
-                <Composer />
+                <Composer _createPost = { this._createPost } />
                 { postsJSX }
             </section>
         );

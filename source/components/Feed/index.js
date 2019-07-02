ce8f82a4da568d6192f388751adab59ae9be4d21
olegleshiy <olegleshiy7@gmail.com>
2019-07-03 01:31:39 +0300

    wrote test for getFullApiUrl

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -11,6 +11,7 @@ import Composer from '../Composer';
 import Post from '../Post';
 import Spinner from '../Spinner';
 import Postman from '../Postman';
+import Counter from '../Counter';
 
 //Instruments
 import Styles from './styles.m.css';
@@ -199,6 +200,7 @@ class Feed extends Component{
                     onEnter = { this._animateComposerEnter }>
                     <Composer _createPost = { this._createPost } />
                 </Transition>
+                <Counter count = { postsJSX.length }/>
                 <Transition
                     in
                     appear
---
+++ source/instruments/index.test.js
@@ -0,0 +1,56 @@
+//Core
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
+
+
+describe('instruments:', () => {
+    test('sum function should be a function', () => {
+        expect(sum).toBeInstanceOf(Function);
+    });
+
+    test('sum function should throw, when called with non-number type as second argument', () => {
+        expect(() => sum(2, 'привет')).toThrow();
+    });
+
+    test('sum function should throw, when called with non-number type as first argument', () => {
+        expect(() => sum('привет', 2)).toThrow();
+    });
+
+    test('sum function should return an addition of two arguments passed', () => {
+        expect(sum(2, 3)).toBe(5);
+        expect(sum(1, 8)).toMatchSnapshot();
+    });
+
+    test('delay function should return a resolved promise', async () => {
+        await expect(delay()).resolves.toBeUndefined();
+    });
+
+    test('getUniqueID function should be a function', () => {
+        expect(getUniqueID).toBeInstanceOf(Function);
+    });
+
+    test('getUniqueID function should throw, when called with non-number type as argument', () => {
+        expect(() => getUniqueID('привет')).toThrow();
+    });
+
+    test('getUniqueID function should produce a string of a desired given length', () => {
+        expect(typeof getUniqueID()).toBe('string');
+        expect(getUniqueID(5)).toHaveLength(5);
+        expect(getUniqueID(13)).toHaveLength(13);
+    });
+
+    test('getFullApiUrl function should be a function', () => {
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
+
+    test('getFullApiUrl function should throw, when called with non-string type as second argument', () => {
+        expect(() => getFullApiUrl('api', 7)).toThrow();
+    });
+
+    test('getFullApiUrl function should throw, when called with non-string type as first argument', () => {
+        expect(() => getFullApiUrl(7, 'GROUP_ID')).toThrow();
+    });
+
+    test('getFullApiUrl function should return a string', () => {
+        expect(getFullApiUrl('api', 'GROUP_ID')).toMatchSnapshot();
+    });
+});
